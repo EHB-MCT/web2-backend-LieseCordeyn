c1453015ab248f980fea2bde2c0f2dfc719d4be3
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-12-13 21:19:37 +0100

    made get request with mongoDB

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const express = require('express')
-const fs = require('fs/promises')
-const app = express()
-const port = 3000
-const bodyParser = require('body-parser')
+const express = require('express');
+const fs = require('fs/promises');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const {
+    MongoClient
+} = require('mongodb');
+const config = require('./config.json');
+const cors = require("cors");
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
+//create mongo client
+const client = new MongoClient(config.finalUrl);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(300).redirect('/info.html')
+    res.status(300).redirect('/info.html');
 })
 
 //return all books
 app.get('/books', async (req, res) => {
     try {
-        //read data
-        let books = await fs.readFile('data/books.json');
+        await client.connect();
+
+        const coll = client.db('courseProject').collection('books');
+        const books = await coll.find({}).toArray();
+
+
         //send data
-        res.status(200).send(JSON.parse(books));
+        res.status(200).send(books);
     } catch (error) {
-        res.status(500).send('File could not be read! Try again later');
+        console.log(error)
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    } finally {
+        await client.close()
     }
 })
 
 //return one book with id
-app.get('/book',async(req, res)=>{
+app.get('/book', async (req, res) => {
     console.log(req.query)
 
     try {
@@ -34,15 +53,15 @@ app.get('/book',async(req, res)=>{
         //find data
         let book = books[req.query.id];
 
-        if(book){
+        if (book) {
             //send data
             res.status(200).send(book);
-        }else{
-            res.status(400).send('boardgame could not be found with id:' + req.query.id );
+        } else {
+            res.status(400).send('boardgame could not be found with id:' + req.query.id);
         }
 
-        
-       
+
+
     } catch (error) {
         res.status(500).send('File could not be read! Try again later');
     }
